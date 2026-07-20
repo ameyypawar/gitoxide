3c588fb37e2d74bcd0f51c9efded5b4ae6671cb6
Amey Pawar <ameyap007aaa@gmail.com>
2026-07-20 21:46:33 +0530

    feat!: remove `thiserror` from `gix-diff`

--- gix-diff/tests/diff/index.rs
+++ gix-diff/tests/diff/index.rs
@@ -1345,7 +1345,8 @@ mod util {
                 .map(|p| gix_pathspec::Pattern::from_bytes(p.as_bytes(), Default::default()).expect("valid pattern")),
             None,
             &root,
-        )?;
+        )
+        .map_err(gix_pathspec::normalize::Error::into_error)?;
         Ok((lhs, rhs, cache, odb, pathspecs))
     }
 
